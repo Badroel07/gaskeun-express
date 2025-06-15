a249af2c337afa11ceb531a854ae5a0c5deb8085
mrzack18 <141796022+mrzack18@users.noreply.github.com>
2025-06-15 23:12:03 +0700

    Delete app directory

--- app/views/kota/index.php
+++
@@ -1,8 +0,0 @@
-<div class="space-y-6 w-full">
-    <h2 class="text-2xl font-semibold text-gray-800 mb-6">Daftar Kota</h2>
-    <ul>
-        <?php foreach ($data['kota'] as $kota): ?>
-            <li><?= $kota['nama_kota'] ?></li>
-        <?php endforeach; ?>
-    </ul>
-</div>
