de3099bda50b624f339f281d412eb83d5e95469f
mrzack18 <141796022+mrzack18@users.noreply.github.com>
2025-06-16 16:01:18 +0700

    Update Notifikasi_model.php

--- app/models/Notifikasi_model.php
+++ app/models/Notifikasi_model.php
@@ -11,7 +11,24 @@ class Notifikasi_model
 
     public function getAllNotifikasi()
     {
-        $this->db->query('SELECT * FROM semua_log ORDER BY tanggal DESC');
+        $this->db->query("
+        SELECT 'log_kota' AS sumber, id_log, aksi, tanggal, status FROM log_kota
+        UNION ALL
+        SELECT 'log_kurir' AS sumber, id_log, aksi, tanggal, status FROM log_kurir
+        UNION ALL
+        SELECT 'log_layanan' AS sumber, id_log, aksi, tanggal, status FROM log_layanan
+        UNION ALL
+        SELECT 'log_paket' AS sumber, id_log, aksi, tanggal, status FROM log_paket
+        UNION ALL
+        SELECT 'log_pelacakan' AS sumber, id_log, aksi, tanggal, status FROM log_pelacakan
+        UNION ALL
+        SELECT 'log_penerima' AS sumber, id_log, aksi, tanggal, status FROM log_penerima
+        UNION ALL
+        SELECT 'log_pengirim' AS sumber, id_log, aksi, tanggal, status FROM log_pengirim
+        UNION ALL
+        SELECT 'log_tarif' AS sumber, id_log, aksi, tanggal, status FROM log_tarif
+        ORDER BY tanggal DESC
+    ");
         return $this->db->resultSet();
     }
 
